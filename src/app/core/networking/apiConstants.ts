5087d6958c9e2929b2d2bda540a2589f248a13e4
3braa04 <braabormo@gmail.com>
2026-03-07 02:19:08 +0300

    fix api url

--- src/app/core/networking/apiConstants.ts
+++ src/app/core/networking/apiConstants.ts
@@ -1,6 +1,6 @@
 export default class ApiConstants
 {
-    // static baseUrl:string = "https://yusrbus.runasp.net/api";
+    static baseUrl:string = "https://yusrbus.runasp.net/api";
     // static baseUrl:string = "https://api.bus.yusrsys.com/api";
-    static baseUrl:string = "https://localhost:7226/api";
+    // static baseUrl:string = "https://localhost:7226/api";
 }
